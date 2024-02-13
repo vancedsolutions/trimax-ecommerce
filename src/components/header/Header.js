5d0aac7782b9f3d69bb1a745accd81ec045e98de
Sanjay Kumar <SanjayKumar@vancedsolutions.com>
2024-02-13 19:11:28 +0530

    Project upload

---
+++ src/components/header/Header.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import CartIcon from "../cart-overlay/CartIcon.js";
+import SuccessMessage from "../SuccessMessage.js";
+import CategoryMenu from "./CategoryMenu.js";
+import CurrencyIcon from "../currency-overlay/CurrencyIcon.js";
+import "./header.css";
+import inactiveHamburger from "../../assets/images/inactive-hamburger.png";
+import activeHamburger from "../../assets/images/active-hamburger.png";
+import Logo from "../../assets/images/logo/trimix.png";
+
+const Navigation = ({
+  allCategories,
+  changeCategory,
+  activeCategory,
+  selectedCurrency,
+  allCurrencies,
+  changeCurrency,
+  amountOfItems,
+  totalPayment,
+  productsQuantity,
+  handleRemoveProduct,
+  handleAddProduct,
+  cartItems,
+  clearCart,
+}) => {
+  const [activeMenu, setInactiveMenu] = useState(false);
+  const [colorChange, setColorChange] = useState(false);
+  const changeNavbarColor = () => {
+    if (window.scrollY >= 80) {
+      setColorChange(true);
+    } else {
+      setColorChange(false);
+    }
+  };
+  window.addEventListener("scroll", changeNavbarColor);
+  const toggleMenu = () => {
+    setInactiveMenu(!activeMenu);
+  };
+  const closeMenu = () => setInactiveMenu(false);
+
+  return (
+    <header>
+      <img
+        className="hamburger"
+        src={activeMenu ? activeHamburger : inactiveHamburger}
+        alt="toggle menu"
+        onClick={toggleMenu}
+      />
+      <nav
+        className={
+          activeMenu ? "active-menu" : colorChange ? "colorChange" : ""
+        }
+      >
+        <NavLink
+          to="/"
+          onClick={() => {
+            changeCategory("");
+            closeMenu();
+          }}
+          className="header-one"
+        >
+          <img
+            src={Logo}
+            alt="toggle menu"
+            onClick={toggleMenu}
+            height={"39px"}
+            width={"100%"}
+          />
+        </NavLink>
+        <CategoryMenu
+          allCategories={allCategories}
+          changeCategory={changeCategory}
+          activeCategory={activeCategory}
+          closeMenu={closeMenu}
+        />
+        <section className="icons-section">
+          <CurrencyIcon
+            selectedCurrency={selectedCurrency}
+            allCurrencies={allCurrencies}
+            changeCurrency={changeCurrency}
+          />
+
+          <CartIcon
+            handleRemoveProduct={handleRemoveProduct}
+            handleAddProduct={handleAddProduct}
+            productsQuantity={productsQuantity}
+            amountOfItems={amountOfItems}
+            totalPayment={totalPayment}
+            cartItems={cartItems}
+            selectedCurrency={selectedCurrency}
+            activeMenu={activeMenu}
+            clearCart={clearCart}
+            closeMenu={closeMenu}
+          />
+        </section>
+      </nav>
+      <SuccessMessage />
+    </header>
+  );
+};
+
+export default Navigation;
